060991eda733a53797ec7c08dc6129bc15382381
Will <mail@williamarnold.info>
2025-01-10 14:37:10 -0500

    Try once again to fix neovim plugins

--- utils/nvim/lua/will/plugins/catppuccin.lua
+++ utils/nvim/lua/will/plugins/catppuccin.lua
@@ -5,11 +5,6 @@ return {
         priority = 1000,
 
         config = function()
-            -- Load Theme
-            require("catppuccino").setup({
-                colorscheme = "dark_catppuccino",
-            })
-
             -- Load Scheme
             vim.cmd([[colorscheme catppuccin]])
         end,
